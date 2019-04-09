58ffe5dcb32de7518be2249347c797373e4f00ac
UrgedTaylor <tjadam1993@gmail.com>
2019-04-08 20:34:32 -0700

    Got all clock speeds and memory working

--- app/src/main/java/com/example/taylor/hardwarespecs/CPUSpecs.java
+++ app/src/main/java/com/example/taylor/hardwarespecs/CPUSpecs.java
@@ -4,6 +4,7 @@ package com.example.taylor.hardwarespecs;
         import android.os.Build;
         import android.os.Bundle;
         import android.os.Debug;
+        import android.os.Handler;
         import android.support.annotation.NonNull;
         import android.support.annotation.Nullable;
         import android.support.v4.app.Fragment;
@@ -16,99 +17,94 @@ package com.example.taylor.hardwarespecs;
         import android.view.ViewGroup;
         import android.widget.TextView;
 
-        import java.io.BufferedReader;
-        import java.io.DataInputStream;
-        import java.io.File;
-        import java.io.FileFilter;
-        import java.io.FileInputStream;
-        import java.io.IOException;
-        import java.io.InputStream;
-        import java.io.InputStreamReader;
-        import java.io.RandomAccessFile;
+
         import java.util.ArrayList;
         import java.util.Timer;
         import java.util.TimerTask;
-        import java.util.regex.Matcher;
-        import java.util.regex.Pattern;
 
-        import static android.content.Context.ACTIVITY_SERVICE;
-        import static android.support.v4.content.ContextCompat.getSystemService;
 
 public class CPUSpecs extends Fragment {
     private  static  final  String TAG = "SystemSpecsFragment";
-    private static final Build build = new Build();
     private ArrayList<InfoObject> mInfoObject;
-    TextView textView ;
     InfoUtil mInfoUtil = new InfoUtil();
+    RecyclerViewAdapter adapter;
+    private TimerTask mTimerTask;
+    private Timer timer = new Timer();
+    private Handler timerHandler;
+    private int numCpus;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view =  inflater.inflate(R.layout.tab1_fragment, container, false);
+        numCpus = mInfoUtil.getNumCores();
+        timerHandler = new Handler();
         initInfoObjects(view);
+        onTimerTick(view);
+        timer.schedule(mTimerTask, 10, 2500);
         return view;
     }
 
-
-
-//    private String readFile()
-//    {
-//        String myData = "";
-//        File myExternalFile = new File("proc/cpuinfo");
-//        try {
-//            FileInputStream fis = new FileInputStream(myExternalFile);
-//            DataInputStream in = new DataInputStream(fis);
-//            BufferedReader br = new BufferedReader(new InputStreamReader(in));
-//
-//            String strLine;
-//            Boolean read = false;
-//                while ((strLine = br.readLine()) != null) {
-//                    if(strLine.contains("Hardware")) read = true;
-//                    if(read) {
-//                        Log.d(TAG, "readFile: " + strLine.substring(strLine.indexOf(':')));
-//                        myData = myData + strLine + "\n";
-//                        read = false;
-//                    }
-//                    }
-//            br.close();
-//            in.close();
-//            fis.close();
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-//        return myData + "\n " + getNumCores();
-//    }
-
-
+    public void onTimerTick(View view) {
+        final View view2 = view;
+        mTimerTask = new TimerTask() {
+            //this method is called every 1ms
+            public void run() {
+                timerHandler.post(new Runnable() {
+                    public void run() {
+                        //update textView
+                        //ERROR:textView2 cannot be resolved
+                        double[] ms = mInfoUtil.getMemorySize(view2);
+                      setInfoObject("Free Memory", String.format("%.2f",ms[1]/1048576) + " MB");
+                      setInfoObject("Available Memory", String.format("%.2f",(ms[0]/1048576) - ms[1]/1048576) + " MB");
+                      for(int i = 1; i <= numCpus; i++)
+                        setInfoObject("       Cpu"+i,Double.toString(mInfoUtil.getFrequency(i-1)/1000) + " MHz");
+                    }
+                });
+            }};
+    }
 
 
     private void initInfoObjects(View view) {
-       long[] ms = mInfoUtil.getMemorySize(view);
-
+       double[] ms = mInfoUtil.getMemorySize(view);
+       double[] clockSpeeds = mInfoUtil.getClockSpeeds(numCpus);
         mInfoObject = new ArrayList<>();
         mInfoObject.add(new InfoObject(
                 "Total Memory",
-                Long.toString(ms[0]/1048576) + " MB"
+                String.format("%.2f", ms[0]/1048576) + " MB"
         ));
         mInfoObject.add(new InfoObject(
                 "Free Memory",
-                Long.toString(ms[1]/1048576) + " MB"
+                String.format("%.2f", ms[1]/1048576) + " MB"
+        ));
+
+        mInfoObject.add(new InfoObject(
+                "Available Memory",
+                String.format("%.2f",(
+                        ms[0] - ms[1]
+                ))
         ));
         mInfoObject.add(new InfoObject(
                 "Number of Cores",
                 Integer.toString(mInfoUtil.getNumCores())
         ));
         mInfoObject.add(new InfoObject(
-                "Test",
-                "Test"
+                "Clock Speed Range",
+                Double.toString(clockSpeeds[0]/1000) + " MHz - " + String.format("%.2f", clockSpeeds[1]/1000.00)+ " GHz"
         ));
+        for(int i = 1; i <= numCpus; i++) {
+            mInfoObject.add(new InfoObject(
+                    "       Cpu"+i,
+                    Double.toString(mInfoUtil.getFrequency(i-1)/1000) + " MHz"
+            ));
+        }
 
         initRecyclerView(view);
     }
     private void initRecyclerView(View view) {
         Log.d(TAG, "initRecyclerView: ");
         RecyclerView recyclerView = view.findViewById(R.id.recyclerview);
-        RecyclerViewAdapter adapter = new RecyclerViewAdapter(this.getContext(), mInfoObject);
+        adapter = new RecyclerViewAdapter(this.getContext(), mInfoObject);
         recyclerView.setAdapter(adapter);
         LinearLayoutManager layoutManager = new LinearLayoutManager(this.getActivity());
         recyclerView.setLayoutManager(layoutManager);
@@ -122,6 +118,19 @@ public class CPUSpecs extends Fragment {
                 return io;
 
         return null;
+    }
+    private void setInfoObject(String name, String info) {
+        for (int i = 0; i < this.mInfoObject.size(); i++){
+            InfoObject io = mInfoObject.get(i);
+            if (io.getName().equals(name)) {
+                io.setInfo(info);
+                adapter.notifyItemChanged(i);
+
+            }
+    }
+
+
+
     }
 
 
--- app/src/main/java/com/example/taylor/hardwarespecs/InfoUtil.java
+++ app/src/main/java/com/example/taylor/hardwarespecs/InfoUtil.java
@@ -2,9 +2,16 @@ package com.example.taylor.hardwarespecs;
 
 import android.app.ActivityManager;
 import android.os.Build;
+import android.util.Log;
 import android.view.View;
 
 
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.regex.Pattern;
 
 import static android.content.Context.ACTIVITY_SERVICE;
@@ -12,11 +19,12 @@ import static android.content.Context.ACTIVITY_SERVICE;
 /*Helper class for returning various details about the device*/
 
 public class InfoUtil {
+    private static final String TAG = "InfoUtil";
 
 
      /* Returns the current memory usage and free memory in bytes */
-    public long[] getMemorySize(View view) {
-        long[] res = new long[2];
+    public double[] getMemorySize(View view) {
+        double[] res = new double[2];
         ActivityManager actManager = (ActivityManager) view.getContext().getSystemService(ACTIVITY_SERVICE);
         ActivityManager.MemoryInfo memInfo = new ActivityManager.MemoryInfo();
         actManager.getMemoryInfo(memInfo);
@@ -25,6 +33,13 @@ public class InfoUtil {
 
 
         return res;
+    }
+    public double getFreeMemory(View view) {
+        ActivityManager actManager = (ActivityManager) view.getContext().getSystemService(ACTIVITY_SERVICE);
+        ActivityManager.MemoryInfo memInfo = new ActivityManager.MemoryInfo();
+        actManager.getMemoryInfo(memInfo);
+        return memInfo.availMem;
+
     }
     public int getNumCores() {
         return Runtime.getRuntime().availableProcessors();
@@ -41,6 +56,88 @@ public class InfoUtil {
         else if(release<9)codeName="Oreo";
         else codeName="Pie";
         return codeName+" v"+release+", API Level: "+Build.VERSION.SDK_INT;
+    }
+
+        public String readFile()
+    {
+        String myData = "";
+        File myExternalFile = new File("proc/cpuinfo");
+        try {
+            FileInputStream fis = new FileInputStream(myExternalFile);
+            DataInputStream in = new DataInputStream(fis);
+            BufferedReader br = new BufferedReader(new InputStreamReader(in));
+
+            String strLine;
+            Boolean read = false;
+                while ((strLine = br.readLine()) != null) {
+                    if(strLine.contains("Hardware")) read = true;
+                    if(read) {
+
+                        myData = myData + strLine + "\n";
+                        read = false;
+                    }
+                    }
+            br.close();
+            in.close();
+            fis.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return myData + "\n " + getNumCores();
+    }
+    public double getFrequency(int cpuNumber){
+
+        double myData = 0;
+        File myExternalFile = new File("/sys/devices/system/cpu/cpu"+cpuNumber+"/cpufreq/scaling_cur_freq");
+
+        try {
+            FileInputStream fis = new FileInputStream(myExternalFile);
+            DataInputStream in = new DataInputStream(fis);
+            BufferedReader br = new BufferedReader(new InputStreamReader(in));
+
+            String strLine;
+
+           strLine = br.readLine();
+           // Log.d(TAG, "getFrequency: " + strLine );
+           myData = Long.parseLong(strLine);
+
+            br.close();
+            in.close();
+            fis.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return myData;
+    }
+    public double[] getClockSpeeds(int numCpus){
+
+        double myData[] = {0, 0};
+        File maxFreq = new File("/sys/devices/system/cpu/cpu7/cpufreq/scaling_max_freq");
+        File minFreq = new File("/sys/devices/system/cpu/cpufreq/policy0/cpuinfo_min_freq");
+        File[] files = {minFreq, maxFreq};
+        for (int i = 0; i < files.length; i++) {
+            try {
+                File myExternalFile = files[i];
+                FileInputStream fis = new FileInputStream(myExternalFile);
+                DataInputStream in = new DataInputStream(fis);
+                BufferedReader br = new BufferedReader(new InputStreamReader(in));
+
+                String strLine;
+
+                strLine = br.readLine();
+                Log.d(TAG, "getFrequency: " + strLine);
+                myData[i] = Long.parseLong(strLine);
+
+                br.close();
+                in.close();
+                fis.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        myData[1]/=1000;
+
+        return myData;
     }
 
 }
